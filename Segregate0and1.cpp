f3c239fd0d8061e1d167d65ad91cd5752a757ee4
Shammi-139 <shammidhl139@gmail.com>
2026-09-21 14:55:17 +0530

    Create Segregate0and1.cpp

---
+++ Segregate0and1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void segregate0and1(vector<int> &arr) {
+        int i = 0;
+        int j = arr.size() - 1;
+
+        while(i < j) {
+
+            if(arr[i] == 0) {
+                i++;
+            }
+            else if(arr[j] == 1) {
+                j--;
+            }
+            else {
+                // arr[i] = 1 and arr[j] = 0
+                swap(arr[i], arr[j]);
+                i++;
+                j--;
+            }
+        }
+    }
+};
